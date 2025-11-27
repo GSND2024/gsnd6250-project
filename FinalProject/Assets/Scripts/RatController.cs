d0a34b17cb50c3715ec2a6a85ea29fa478905595
Bertoh14 <robbyoh@gmail.com>
2025-11-26 20:34:08 -0800

    Added Rat Maze
    Rat Maze

---
+++ FinalProject/Assets/Scripts/RatController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RatController : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float turnSpeed = 180f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Make sure your Project Settings → Player → Active Input Handling is set to "Both" or "Input Manager"
+        float horizontal = Input.GetAxis("Horizontal"); // A / D or Left / Right
+        float vertical = Input.GetAxis("Vertical");   // W / S or Up / Down
+
+        // Turn left/right
+        transform.Rotate(0f, horizontal * turnSpeed * Time.fixedDeltaTime, 0f);
+
+        // Move forward/back
+        Vector3 move = transform.forward * vertical * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
+    }
+}
