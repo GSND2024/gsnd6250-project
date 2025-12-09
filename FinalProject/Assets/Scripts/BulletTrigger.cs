873c4daf4c268fc9bd07087110c854c3d2ae41d4
Stephen Singler <stephen.singler@gmail.com>
2025-12-09 13:55:46 -0800

    add props to hedgemaze and bullet spin

--- FinalProject/Assets/Scripts/BulletTrigger.cs
+++ FinalProject/Assets/Scripts/BulletTrigger.cs
@@ -2,6 +2,29 @@ using UnityEngine;
 
 public class BulletTrigger : MonoBehaviour
 {
+    [Header("Spin Settings")]
+    public Vector3 rotationSpeed = new Vector3(0f, 60f, 0f); // Degrees per second
+
+    [Header("Bob Settings")]
+    public float bobAmplitude = 0.25f; // How high/low it moves
+    public float bobFrequency = 2f;    // Speed of bobbing
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Spin object
+        transform.Rotate(rotationSpeed * Time.deltaTime);
+
+        // Bob up and down
+        float newY = startPos.y + Mathf.Sin(Time.time * bobFrequency) * bobAmplitude;
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
